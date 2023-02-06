ca71c655ed63d20a4ca562912de48da5f60e0a3d
Aayush Vyas <aayvyas@publicisgroupe.net>
2023-02-06 11:06:21 +0530

    practise sample and updated README.md

---
+++ src/main.groovy
@@ -0,0 +1,20 @@
+println "Hello, World!"
+
+
+List evens = []
+// Taking x
+def evenTillX = { x -> 
+    
+    List evensCalc = (0..x).collect { i ->
+        if(i%2==0){
+            // pushing i to evens List
+            evens << i
+        }
+    }
+    return evens
+}
+
+evenTillX(10)
+
+println evens
+
